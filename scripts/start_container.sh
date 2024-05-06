1681550794ff52fbc3cf1438e588fc3eae46a1df
Karthik351 <152361790+Karthik351@users.noreply.github.com>
2024-05-05 23:28:58 -0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull karthik3513/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 karthik3513/simple-python-flask-app:latest 
+docker run -d -p 8000:8000 karthik3513/simple-python-flask-app:latest  
